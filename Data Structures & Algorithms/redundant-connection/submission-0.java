973c2695cec085040f511dd9b5bf6d6f1ee1bc0b
Satyam Gupta <38059761+sagupta1001@users.noreply.github.com>
2026-08-04 13:08:27 -0400

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.java
@@ -0,0 +1,51 @@
+class Solution {
+    // problem
+    // return an edge that can be removed so that the graph
+    // is still connected but no cycles
+    // return the edge that appears last if multiple answers
+
+    // approach
+    // there was an algorithm (not djikstra's) that helped find a cycle
+    // perhaps we lead with that?
+
+
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i <= n; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        for (int[] edge: edges) {
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+
+            boolean[] visit = new boolean[n+1];
+
+            if (dfs(u, -1, adj, visit)) {
+                return edge;
+            }
+        }
+
+        return new int[0];
+    }
+
+    private boolean dfs(int node, int parent, List<List<Integer>> adj, boolean[] visit) {
+        if (visit[node]) {
+            return true;
+        }
+
+        visit[node] = true;
+        for (int nei : adj.get(node)) {
+            if (nei == parent) {
+                continue;
+            }
+            if (dfs(nei, node, adj, visit)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
